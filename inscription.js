35849384c0fcd22e4b3b78df0bd26a2dee72e594
jacques zhu <jacques.zhu19@gmail.com>
2023-02-20 16:30:48 +0100

    update

---
+++ inscription.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const pages = document.querySelectorAll(".page");
+    const numpage = document.querySelector(".numpage");
+    const nbPages = pages.length;
+    let pageActive = 1;
+    
+    console.log(nbPages);
+  });
+  
