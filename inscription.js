1a98b1635826ae45c1bea63a8d904cff9a41eed4
jacques zhu <jacques.zhu19@gmail.com>
2023-02-21 15:18:10 +0100

    update

--- inscription.js
+++ inscription.js
@@ -1,15 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
   const pages = document.querySelectorAll(".page");
-  const numpage = document.querySelector(".numpage");
+  const suivant = document.querySelector(".next");
+  const precedent = document.querySelector(".prev");
   const nbPages = pages.length;
   let pageActive = 1;
 
   // On affiche la 1ère page du formulaire
   pages[0].style.display = "initial";
-
-  // On récupère les boutons "Suivant" et "Précédent"
-  const suivant = document.querySelector(".next");
-  const precedent = document.querySelector(".prev");
 
   // On ajoute l'écouteur d'événement sur le bouton "Suivant"
   suivant.addEventListener("click", function() {
@@ -19,9 +16,6 @@ document.addEventListener("DOMContentLoaded", function() {
     pageActive++;
     // On affiche la page suivante
     pages[pageActive - 1].style.display = "initial";
-    // On met à jour le numéro de la page active dans l'entête
-    document.querySelector(".page-num.active").classList.remove("active");
-    document.querySelector("#num" + pageActive).classList.add("active");
     // On masque le bouton "Suivant" sur la dernière page
     if (pageActive === nbPages) {
       suivant.style.display = "none";
@@ -40,9 +34,6 @@ document.addEventListener("DOMContentLoaded", function() {
     pageActive--;
     // On affiche la page précédente
     pages[pageActive - 1].style.display = "initial";
-    // On met à jour le numéro de la page active dans l'entête
-    document.querySelector(".page-num.active").classList.remove("active");
-    document.querySelector("#num" + pageActive).classList.add("active");
     // On masque le bouton "Précédent" sur la première page
     if (pageActive === 1) {
       precedent.style.display = "none";
